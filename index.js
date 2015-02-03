f23cc9c87488575d06d0f1086deee0e6be02d046
Brian Woodward <brian.woodward@gmail.com>
2015-02-02 22:21:07 -0500

    setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * transform-cache <https://github.com/doowb/transform-cache>
+ *
+ * Copyright (c) 2015 Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
